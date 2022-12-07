dd7011609f6c7eaace3fd7458fcd86f8c78cdaeb
elbica <sh0705k@gmail.com>
2022-12-08 01:48:04 +0900

    :bug: Fix: 사용하지 않는 style class 제거

--- src/components/common/Navigation/index.tsx
+++ src/components/common/Navigation/index.tsx
@@ -54,10 +54,10 @@ function Navigation({
   return (
     <nav className="sticky top-0 flex min-h-[5rem] w-full items-center justify-between gap-16 bg-white">
       <div className="flex grow items-center gap-16">{left}</div>
-      <span className="text-space absolute flex h-full w-full items-center justify-center text-regular">
+      <span className="absolute flex h-full w-full items-center justify-center text-header">
         {title}
       </span>
-      <div className="text flex w-fit items-center justify-center gap-8">{right}</div>
+      <div className="flex w-fit items-center justify-center gap-8">{right}</div>
     </nav>
   );
 }

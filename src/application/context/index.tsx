5360817ca97ecce2473ef7c835ae8db9631e586b
elbica <sh0705k@gmail.com>
2022-12-07 20:49:53 +0900

    :fire: Remove: Context 폴더 삭제

--- src/application/context/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { PropsWithChildren } from "react";
-
-import { NavigationContextProvider } from "@/application/context/Navigation";
-
-const ContextProvider = ({ children }: PropsWithChildren) => {
-  return <NavigationContextProvider>{children}</NavigationContextProvider>;
-};
-
-export default ContextProvider;

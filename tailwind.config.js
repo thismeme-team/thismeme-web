f6ed6e281658125282388ac5956ddb7fe75fd4fe
Jeongjin Oh <ojj991123@gmail.com>
2022-12-21 16:38:14 +0900

    📦 chore: minWidth,maxWidth,minHeight 0-300px 커스텀

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ module.exports = {
     extend: {
       spacing: PX0_300,
       borderRadius: PX0_50,
+      minWidth: PX0_300,
+      maxWidth: PX0_300,
+      minHeight: PX0_300,
       colors: {
         "light-gray-10": "#f6f6f6",
         "light-gray-20": "#eeeeee",
@@ -88,6 +91,10 @@ module.exports = {
           "0%": { height: "var(--radix-accordion-content-height)" },
           "100%": { height: 0 },
         },
+      },
+      animation: {
+        "slide-down": "slideDown 300ms cubic-bezier(0.87,0,0.13,1)",
+        "slide-up": "slideUp 300ms cubic-bezier(0.87,0,0.13,1)",
       },
     },
   },

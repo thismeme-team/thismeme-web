65e7daee22d832f9e813233bb2aec1331366572c
elbica <sh0705k@gmail.com>
2023-01-03 03:19:27 +0900

    :lipstick: Style: Toast Container FLIP animation [YW-127]

--- src/components/common/Toast/Toast.tsx
+++ src/components/common/Toast/Toast.tsx
@@ -4,10 +4,11 @@ import { toastColor, toastIconColor } from "@/components/common/Toast/types";
 
 export const Toast = ({ message, className = "", icon, id, color = "black", visible }: Props) => {
   return (
+    // NOTE text를 완전히 center에 두기 위해 line-height를 0으로 주었음
     <output
       className={`flex min-h-38 min-w-197 ${
         visible ? "animate-enter" : "animate-exit"
-      } items-center justify-center gap-4 rounded-5 px-10 py-7 text-15-semibold-130 leading-none backdrop-blur-[2px] ${
+      } items-center justify-center gap-4 rounded-5 px-10 py-7 text-15-semibold-130 leading-none backdrop-blur-[2px] will-change-transform ${
         toastColor[color]
       } ${className}`}
     >
--- src/components/common/Toast/ToastContainer.tsx
+++ src/components/common/Toast/ToastContainer.tsx
@@ -1,8 +1,11 @@
-import { useRef } from "react";
+import { useLayoutEffect, useRef } from "react";
 
 import { Portal } from "@/components/common/Portal";
 import { useToastContext } from "@/components/common/Toast/context";
 import { Toast } from "@/components/common/Toast/Toast";
+
+let prevHeight = 0;
+const EXPAND_DELAY = 230;
 
 export const ToastContainer = () => {
   const toasts = useToastContext();
@@ -10,13 +13,32 @@ export const ToastContainer = () => {
   const ref = useRef<HTMLElement>(null);
 
   /**
-   * TODO FLIP Container Animation
+   * NOTE FLIP Animation
+   *  @link https://web.dev/building-a-toast-component/#managing-one-or-many-toasts
    */
+  useLayoutEffect(() => {
+    if (ref.current) {
+      const currentHeight = ref.current.getBoundingClientRect().height;
+
+      if (currentHeight > prevHeight) {
+        const invert = currentHeight - prevHeight;
+
+        ref.current.animate(
+          [{ transform: `translateY(${invert}px)` }, { transform: "translateY(0)" }],
+          {
+            duration: EXPAND_DELAY,
+            easing: "ease-out",
+          },
+        );
+      }
+      prevHeight = currentHeight;
+    }
+  }, [toasts]);
 
   return (
     <Portal id="toast-portal">
       <aside
-        className="pointer-events-none fixed inset-18 z-10 grid place-items-center content-end gap-6"
+        className="pointer-events-none fixed inset-x-18 bottom-18 z-10 grid place-items-center content-end gap-6"
         ref={ref}
       >
         {toasts.map((t) => (

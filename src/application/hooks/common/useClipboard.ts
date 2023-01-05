ddb6e2dbeb8e7707d256409239e2dcc5cb8ff487
elbica <sh0705k@gmail.com>
2023-01-05 23:59:51 +0900

    :sparkles: Feat: 클립보드 미지원 시 onError 콜백 실행 [YW-110]

--- src/application/hooks/common/useClipboard.ts
+++ src/application/hooks/common/useClipboard.ts
@@ -2,15 +2,15 @@ import { useCallback } from "react";
 
 const NOT_SUPPORT_CLIPBOARD = "클립보드를 지원하지 않습니다";
 
-interface Options {
+interface WriteTextOptions {
   onSuccess?: () => void;
   onError?: () => void;
 }
 
 export const useClipboard = () => {
-  const writeText = useCallback((text: string, options?: Options) => {
+  const writeText = useCallback((text: string, options?: WriteTextOptions) => {
     if (typeof navigator.clipboard === "undefined")
-      return Promise.reject<string>(NOT_SUPPORT_CLIPBOARD);
+      return Promise.reject(NOT_SUPPORT_CLIPBOARD).catch(options?.onError);
 
     return navigator.clipboard.writeText(text).then(options?.onSuccess).catch(options?.onError);
   }, []);

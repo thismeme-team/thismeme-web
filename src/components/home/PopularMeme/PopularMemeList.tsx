a8172bb62e9b9cbe7de79536a8f96ab36fbb6608
YoommY <isho48239@gmail.com>
2023-01-19 16:55:37 +0900

    📦 chore : 리뷰 반영

--- src/components/home/PopularMeme/PopularMemeList.tsx
+++ src/components/home/PopularMeme/PopularMemeList.tsx
@@ -1,5 +1,3 @@
-import { useCallback, useMemo } from "react";
-
 import { useGetPopularMeme, useIntersect } from "@/application/hooks";
 import { Masonry } from "@/components/common/Masonry";
 import { MemeItem } from "@/components/meme/MemeItem";
@@ -7,13 +5,11 @@ import { MemeItem } from "@/components/meme/MemeItem";
 export const PopularMemeList = () => {
   const { data, fetchNextPage } = useGetPopularMeme();
 
-  const memeList = useMemo(() => (data ? data.pages.flatMap(({ data }) => data) : []), [data]);
+  const memeList = data ? data.pages.flatMap(({ data }) => data) : [];
 
-  const onIntersect = useCallback(async () => {
+  const ref = useIntersect(async () => {
     fetchNextPage();
-  }, [fetchNextPage]);
-
-  const ref = useIntersect(onIntersect);
+  });
   return (
     <>
       <Masonry columns={2} defaultColumns={2} defaultHeight={450} defaultSpacing={9} spacing={9}>

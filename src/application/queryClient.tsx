ebd613de8785a83c5db9bd61320748e84339fee8
elbica <sh0705k@gmail.com>
2023-01-20 12:16:22 +0900

    :bug: Fix: refetchOnMount 옵션 제거

--- src/application/queryClient.tsx
+++ src/application/queryClient.tsx
@@ -22,7 +22,6 @@ export const QueryClientProvider = ({ hydrateState, children }: Props) => {
             suspense: true,
             retry: 0,
             refetchOnWindowFocus: false,
-            refetchOnMount: false,
             useErrorBoundary: true,
           },
         },

64823adef337468022ca04c983e8dfd33637e71b
elbica <sh0705k@gmail.com>
2023-02-01 16:35:17 +0900

    :bug: Fix: AT localstorage 저장 로직 삭제 [YW-149]

--- src/infra/api/auth/index.ts
+++ src/infra/api/auth/index.ts
@@ -1,18 +1,16 @@
 import type { AxiosInstance, AxiosRequestConfig } from "axios";
 import { isAxiosError } from "axios";
 
-import { IS_CSR, safeLocalStorage } from "@/application/util";
+import { IS_CSR, LOCAL_STORAGE_KEY, safeLocalStorage } from "@/application/util";
 import type { RefreshResponse } from "@/infra/api/auth/types";
-
-const ACCESS_TOKEN_KEY = "accessToken";
 
 export class AuthApi {
   constructor(private api: AxiosInstance) {
     if (!IS_CSR) return;
+    this.setRequestAgainInterceptor(api);
+  }
 
-    const token = safeLocalStorage.get(ACCESS_TOKEN_KEY);
-    if (token) this.setAccessToken(token);
-
+  private setRequestAgainInterceptor = (api: AxiosInstance) => {
     /**
      * @desc
      *  401 에러 시 토큰 갱신 후 이전 api 재요청 합니다
@@ -21,58 +19,46 @@ export class AuthApi {
      *  refresh api가 401을 반환하면 재로그인을 해야 합니다
      *  - AT 삭제 후 페이지 강제 리로드로 일단 처리
      */
-    this.api.interceptors.response.use(null, async (error) => {
+    api.interceptors.response.use(null, async (error) => {
       if (!isAxiosError(error)) return Promise.reject(error);
 
-      const status = error.response?.status;
+      const status = Number(error.response?.status);
       const origin = error.config as AxiosRequestConfig;
 
-      if (Number(status) !== 401 || origin.headers?.retry) return Promise.reject(error);
-
-      if (origin.url === "/token/refresh") {
-        this.deleteAccessToken();
+      if (origin.url === "/token/refresh" && status === 401) {
+        safeLocalStorage.remove(LOCAL_STORAGE_KEY.isLogin);
         location.reload();
-        return Promise.reject(error);
       }
 
+      if (status !== 401 || origin.headers?.retry) return Promise.reject(error);
+
       const token = await this.refresh();
-      return this.api({
+      return api({
         ...origin,
         headers: { ...origin.headers, authorization: `Bearer ${token}`, retry: true },
       });
     });
-  }
-
-  getAccessToken = () => safeLocalStorage.get(ACCESS_TOKEN_KEY);
-
-  setAccessToken = (token: string) => {
-    safeLocalStorage.set(ACCESS_TOKEN_KEY, token);
-    this.api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
   };
 
-  deleteAccessToken = () => {
-    safeLocalStorage.set(ACCESS_TOKEN_KEY, "");
-    delete this.api.defaults.headers.common["Authorization"];
+  getAccessToken = () =>
+    this.api.defaults.headers.common["Authorization"]?.toString().split(" ").at(-1);
+
+  setAccessToken = (token: string) => {
+    this.api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
   };
 
   logout = () => {
     const accessToken = this.getAccessToken();
 
-    return this.api.post(`/token/blacklist`, { accessToken }).then(this.deleteAccessToken);
+    return this.api.post(`/token/blacklist`, { accessToken });
   };
 
   refresh = async () => {
     const accessToken = this.getAccessToken();
 
-    const { data } = await this.api.post<RefreshResponse>(
-      `/token/refresh`,
-      { accessToken },
-      { withCredentials: true },
-    );
+    const { data } = await this.api.post<RefreshResponse>(`/token/refresh`, { accessToken });
 
     this.setAccessToken(data.accessToken);
     return data.accessToken;
   };
-
-  // requestAgainTest = () => this.api.post("/token/requestAgainTest");
 }

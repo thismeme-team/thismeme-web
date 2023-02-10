2c60283d7dee10fd9914d32883f102f56aa904e1
Jeongjin Oh <ojj991123@gmail.com>
2023-02-11 02:30:10 +0900

    🐛 fix: localFont 옵션 수정 - display: swap 옵션 추가

--- src/styles/fonts/index.ts
+++ src/styles/fonts/index.ts
@@ -4,15 +4,18 @@ export const pretendard = localFont({
   src: "./PretendardVariable.woff2",
   variable: "--font-pretendardVariable",
   weight: "100 900",
+  display: "swap",
 });
 
 export const suit = localFont({
   src: "./SUIT-Variable.woff2",
   variable: "--font-suitVariable",
   weight: "100 900",
+  display: "swap",
 });
 
 export const tossface = localFont({
   src: "./TossFaceFontMac.ttf",
   variable: "--font-tossfaceVariable",
+  display: "swap",
 });

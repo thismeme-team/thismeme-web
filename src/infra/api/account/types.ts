5007e440917ccc931950bd34dbc338eb3cc820e6
YoommY <isho48239@gmail.com>
2023-02-18 04:20:45 +0900

    ✨ feat : account 필드 추가

--- src/infra/api/account/types.ts
+++ src/infra/api/account/types.ts
@@ -3,4 +3,10 @@ export type GetMyAccountResponse = {
   email: string;
   modifiedDate: string | null;
   name: string;
+  imageUrl?: string;
+  shareCount: number;
+  saveCount: number;
+  collectionId: number;
+  shareCollectionId: number;
+  //user_id 추가 예정
 };

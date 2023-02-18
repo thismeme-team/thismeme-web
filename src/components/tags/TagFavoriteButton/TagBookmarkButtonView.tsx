7cacdb36b940fdddfdac28e505f2da739ad83121
elbica <sh0705k@gmail.com>
2023-02-19 05:18:43 +0900

    :lipstick: Style: 태그 북마크 버튼 css 수정

--- src/components/tags/TagFavoriteButton/TagBookmarkButtonView.tsx
+++ src/components/tags/TagFavoriteButton/TagBookmarkButtonView.tsx
@@ -5,18 +5,26 @@ interface Props {
   onClick: () => void;
   checked: boolean;
 }
+
+const animation = "transition-colors duration-200 ease-in-out";
+
 export const TagBookmarkButtonView = ({ onClick, checked }: Props) => {
   return (
     <label className="fixed bottom-32 right-18 text-center">
-      <input checked={checked} className="peer hidden" type="checkbox" />
       <Button
-        className="mb-3 flex h-60 w-60 items-center justify-center rounded-full bg-gray-700 transition-colors duration-200 ease-in-out focus:bg-black peer-checked:bg-primary-300"
         id="bookmark"
+        className={`${
+          checked ? "bg-primary-300" : "bg-gray-700"
+        } ${animation} peer mb-3 h-60 w-60 rounded-full focus:bg-black `}
         onClick={onClick}
       >
         <Icon height={30} name="star" width={30} />
       </Button>
-      <span className="text-12-bold-160 text-gray-700 transition-colors duration-200 ease-in-out peer-checked:text-gray-600 peer-focus:text-black ">
+      <span
+        className={`${
+          checked ? "text-gray-600" : "text-gray-700"
+        } ${animation} text-12-bold-160 peer-focus:text-black`}
+      >
         {checked ? "북마크 완료!" : "태그 북마크"}
       </span>
     </label>

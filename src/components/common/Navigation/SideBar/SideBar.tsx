42d68e97bff6d3da91ef6591a857ff57d054d869
Jeongjin Oh <ojj991123@gmail.com>
2023-03-19 15:01:38 +0900

    💄 style: 로그인 전 홈 메뉴 디자인 [YW-357]

--- src/components/common/Navigation/SideBar/SideBar.tsx
+++ src/components/common/Navigation/SideBar/SideBar.tsx
@@ -2,9 +2,7 @@ import Link from "next/link";
 
 import { Drawer } from "@/components/common/Drawer";
 import { Icon } from "@/components/common/Icon";
-import { Category } from "@/components/common/Navigation/SideBar/Category";
 import { SSRSuspense } from "@/components/common/Suspense";
-import { SearchInput } from "@/components/search";
 
 export const SideBar = () => {
   return (
@@ -12,14 +10,43 @@ export const SideBar = () => {
       <Drawer.Trigger>
         {({ isOpen }) => (isOpen ? <Icon name="cancel" /> : <Icon name="menu" />)}
       </Drawer.Trigger>
-      <Drawer.Content className="mt-54" direction="right">
-        <Link className="mt-8 mb-4 block" href="/search">
-          <SearchInput inputMode="none" placeholder="당신이 생각한 '그 밈' 검색하기" />
-        </Link>
+      <Drawer.Content className="mt-70" direction="right">
         <SSRSuspense>
-          <div className="px-14">
-            <Category />
+          <div className="rounded-24 bg-gray-100 px-16 py-24 pt-16">
+            <div className="flex items-center gap-12">
+              <div className="h-50 w-50 rounded-20 bg-primary-200"></div>
+              <div className="h-full flex-1">로그인하기</div>
+              <Icon name="setting" />
+            </div>
+            <div className="mt-24 flex justify-center divide-x divide-solid divide-gray-200">
+              <Link className="pr-40 text-center" href="/share">
+                <div className="text-32-bold-140">00</div>
+                <div className="text-16-semibold-140">share</div>
+              </Link>
+              <Link className="pl-40 text-center" href="/collect">
+                <div className="text-32-bold-140">00</div>
+                <div className="text-16-semibold-140">collect</div>
+              </Link>
+            </div>
           </div>
+
+          <section className="mt-86 flex flex-col gap-20">
+            <div className="flex items-center justify-between text-16-semibold-140">
+              <span>계정 정보</span>
+              {/* TODO: 유저 정보 API 연동 */}
+              <span className="text-gray-600">ojj1123@naver.com</span>
+            </div>
+            <div className="flex items-center justify-between text-16-semibold-140">
+              <span>공지사항</span>
+            </div>
+            <div className="flex items-center justify-between text-16-semibold-140">
+              {/* TODO: 채널톡 연동 */}
+              <span>게시물 신고</span>
+            </div>
+            <div className="flex items-center justify-between text-16-semibold-140">
+              <span>서비스 이용 문의</span>
+            </div>
+          </section>
         </SSRSuspense>
       </Drawer.Content>
     </Drawer>

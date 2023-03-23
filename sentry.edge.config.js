e9755807a5558a4eb61a68d711cd4899c4b25b66
elbica <sh0705k@gmail.com>
2023-03-24 00:52:51 +0900

    :package: Chore: production 환경만 Sentry 실행
    - @link https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#enabled [YW-387]

--- sentry.edge.config.js
+++ sentry.edge.config.js
@@ -6,8 +6,10 @@ import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-Sentry.init({
-  dsn: SENTRY_DSN,
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
-});
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1.0,
+  });
+}

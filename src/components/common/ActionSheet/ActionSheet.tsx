fb03d77480f74fcf7f4ab869383b8f1f57b2efc8
Jeongjin Oh <ojj991123@gmail.com>
2023-03-31 11:38:44 +0900

    💄 design: 액션시트 디자인 변경 - 안드로이드 디자인 삭제 - 다크모드 디자인 삭제

--- src/components/common/ActionSheet/ActionSheet.tsx
+++ src/components/common/ActionSheet/ActionSheet.tsx
@@ -1,7 +1,5 @@
 import type { ComponentProps, PropsWithChildren } from "react";
-import { css } from "twin.macro";
-
-import { android } from "@/application/util";
+import tw, { css } from "twin.macro";
 
 const DELAY = 300;
 
@@ -9,6 +7,7 @@ export const ActionSheet = ({ children, isOpen }: PropsWithChildren<{ isOpen?: b
   return (
     <div
       css={[
+        tw`text-16-semibold-140`,
         css`
           position: fixed;
 
@@ -21,10 +20,9 @@ export const ActionSheet = ({ children, isOpen }: PropsWithChildren<{ isOpen?: b
           transform: translateY(105%);
           transition: transform ${DELAY}ms cubic-bezier(0, 0.8, 0.34, 1);
         `,
-        !android &&
-          css`
-            padding-bottom: calc(env(safe-area-inset-bottom) + 4rem);
-          `,
+        css`
+          padding-bottom: calc(env(safe-area-inset-bottom) + 4rem);
+        `,
         isOpen &&
           css`
             transform: translateY(0);
@@ -33,34 +31,13 @@ export const ActionSheet = ({ children, isOpen }: PropsWithChildren<{ isOpen?: b
     >
       <div
         css={[
+          tw`divide-solid divide-gray-200 divide-y`,
           css`
-            @media (prefers-color-scheme: dark) {
-              background: #2b2e38;
-            }
+            background: white;
+
+            border-radius: 1rem;
+            width: calc(min(100vw, 48rem) - 3.6rem);
           `,
-          android
-            ? css`
-                background: #e2e9f8;
-
-                padding-inline: 1.8rem;
-                border-radius: 0 0 1.3rem 1.3rem;
-                font-size: 1.65rem;
-                width: min(100vw, 48rem);
-                color: #1b1b1f;
-
-                @media (prefers-color-scheme: dark) {
-                  color: #e3e2e6;
-                }
-              `
-            : css`
-                background: white;
-
-                border-radius: 1rem;
-                font-size: 2rem;
-                padding-inline: 1.6rem;
-                width: calc(min(100vw, 48rem) - 3.6rem);
-                color: #007aff;
-              `,
         ]}
       >
         {children}
@@ -72,11 +49,14 @@ export const ActionSheet = ({ children, isOpen }: PropsWithChildren<{ isOpen?: b
 type ActionSheetButtonProps = ComponentProps<"button">;
 const ActionSheetButton = ({
   children,
-  className,
+  className = "",
   ...rest
 }: PropsWithChildren<ActionSheetButtonProps>) => {
   return (
-    <button className={`flex h-62 items-center ${className}`} {...rest}>
+    <button
+      className={`h-63 w-full first:rounded-t-10 last:rounded-b-10 hover:bg-gray-100 active:bg-gray-100 ${className}`}
+      {...rest}
+    >
       {children}
     </button>
   );

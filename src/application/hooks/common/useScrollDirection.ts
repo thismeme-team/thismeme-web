deee812845978cbb37937224a82141bd92c1a338
elbica <sh0705k@gmail.com>
2023-04-13 05:59:23 +0900

    :bug: Fix: Masonry 스크롤 팅김 수정 [YW-367]

--- src/application/hooks/common/useScrollDirection.ts
+++ src/application/hooks/common/useScrollDirection.ts
@@ -13,11 +13,9 @@ const DELAY = 200;
 export const useScrollDirection = () => {
   const prevScrollY = useRef(0);
   const [direction, setDirection] = useState<DIRECTION>(SCROLL_DIRECTION.up);
-  console.log({ direction });
   useEffect(() => {
     const handleScroll = throttle(() => {
       const currentScrollY = window.scrollY;
-      console.log({ currentScrollY, prevScrollY: prevScrollY.current });
       if (currentScrollY > prevScrollY.current + OFFSET) {
         // 위로 스크롤하는 중
         setDirection(SCROLL_DIRECTION.up);

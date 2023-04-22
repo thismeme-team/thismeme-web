e83f668889901e2b2fd90f70e77aadfc5a948438
Jeongjin Oh <ojj991123@gmail.com>
2023-04-22 20:15:26 +0900

    💄 style(Modal): CSSTransition 에서 fadeInOut 애니메이션으로 수정 - Suspense와 CSSTransition 이슈 때문

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -1,12 +1,10 @@
 import type { PropsWithChildren } from "react";
-import { Children, createContext, isValidElement, useContext, useRef } from "react";
-import { CSSTransition } from "react-transition-group";
-import tw, { css } from "twin.macro";
+import { Children, createContext, isValidElement, useContext } from "react";
 
 import { useClickOutside } from "@/application/hooks";
+import { fadeInOut } from "@/application/util";
 import { Icon } from "@/components/common/Icon";
 
-const DELAY = 300;
 interface ModalProps {
   open: boolean;
   onClose: () => void;
@@ -16,7 +14,6 @@ const ModalContext = createContext<ModalProps>(null as unknown as ModalProps);
 
 export const Modal = ({ children, open, onClose }: PropsWithChildren<ModalProps>) => {
   const ref = useClickOutside({ onClose });
-  const nodeRef = useRef(null);
 
   const reactChildren = Children.toArray(children);
 
@@ -33,42 +30,22 @@ export const Modal = ({ children, open, onClose }: PropsWithChildren<ModalProps>
 
   return (
     <ModalContext.Provider value={{ open, onClose }}>
-      <CSSTransition
-        mountOnEnter
-        unmountOnExit
-        in={open}
-        nodeRef={nodeRef}
-        timeout={{
-          appear: DELAY,
-          exit: DELAY,
+      <div
+        className="fixed inset-0 z-[1300] flex touch-none items-center bg-black/50"
+        css={fadeInOut(open)}
+        onTouchEnd={(event) => {
+          if (event.target === event.currentTarget) event.preventDefault();
         }}
       >
-        <div
-          ref={nodeRef}
-          css={[
-            tw`fixed inset-0 z-[1300] flex touch-none bg-black/50`,
-            css`
-              &.enter-done {
-                opacity: 1;
-              }
-              opacity: 0;
-              transition: opacity ${DELAY}ms;
-            `,
-          ]}
-          onTouchEnd={(event) => {
-            if (event.target === event.currentTarget) event.preventDefault();
-          }}
+        <article
+          className="relative m-auto rounded-24 bg-white shadow-[0_0_20px_rgba(38,37,40,0.2)]"
+          ref={ref}
         >
-          <article
-            className="relative m-auto rounded-24 bg-white shadow-[0_0_20px_rgba(38,37,40,0.2)]"
-            ref={ref}
-          >
-            {header}
-            <section className="px-15">{content}</section>
-            {footer}
-          </article>
-        </div>
-      </CSSTransition>
+          {header}
+          <section className="px-15">{content}</section>
+          {footer}
+        </article>
+      </div>
     </ModalContext.Provider>
   );
 };

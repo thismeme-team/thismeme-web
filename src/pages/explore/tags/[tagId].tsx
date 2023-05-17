8eb83a1e029635d95be6b92c354ab8eb7bb60eac
elbica <sh0705k@gmail.com>
2023-05-17 15:50:55 +0900

    :bug: Fix: Thumbnail SSRSuspense 삭제

--- src/pages/explore/tags/[tagId].tsx
+++ src/pages/explore/tags/[tagId].tsx
@@ -6,7 +6,6 @@ import { DEFAULT_DESCRIPTION, TITLE } from "@/application/util";
 import { ExplorePageNavigation } from "@/components/common/Navigation";
 import { NextSeo } from "@/components/common/NextSeo";
 import { PullToRefresh } from "@/components/common/PullToRefresh";
-import { SSRSuspense } from "@/components/common/Suspense";
 import { MemesByTag, Thumbnail } from "@/components/explore";
 import { TagBookmarkButton } from "@/components/tags";
 
@@ -24,9 +23,7 @@ const ExploreByTagPage: NextPage<Props> = ({ searchQuery, tagId }) => {
 
       <PullToRefresh>
         <div className="mt-12">
-          <SSRSuspense>
-            <Thumbnail tag={searchQuery} />
-          </SSRSuspense>
+          <Thumbnail tag={searchQuery} />
           <MemesByTag searchQuery={searchQuery} />
         </div>
       </PullToRefresh>

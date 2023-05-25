5264e0c7571084e470cbfefa74ab430d2d78a306
elbica <sh0705k@gmail.com>
2023-05-25 19:04:33 +0900

    :fire: Remove: MemeDetailPageNavigation 삭제

--- src/components/common/Navigation/index.ts
+++ src/components/common/Navigation/index.ts
@@ -1,7 +1,6 @@
 export * from "./BackButtonNavigation";
 export * from "./ExplorePageNavigation";
 export * from "./IntroPageNavigation";
-export * from "./MemeDetailPageNavigation";
 export * from "./MyPageNavigation";
 export * from "./Navigation";
 export * from "./SearchPageNavigation";

b4eab3c92ffa889aee6fa918360096a66379162d
Jeongjin Oh <ojj991123@gmail.com>
2023-05-26 21:34:49 +0900

    💄 design: ServiceGuide 컴포넌트 디자인 수정 - aside 태그 사용 - 버튼 위치 스타일 수정

--- src/components/common/Layout/ServiceGuide.tsx
+++ src/components/common/Layout/ServiceGuide.tsx
@@ -8,12 +8,12 @@ export const ServiceGuide = () => {
   const serviceGuide = "/img/serviceGuide.svg";
 
   return (
-    <div className="invisible fixed top-[41%] flex w-[calc((100%-44rem)/2)] justify-end pr-42 xl:visible">
-      <div className="flex flex-col justify-end">
+    <aside className="invisible fixed top-[41%] flex w-[calc((100%-44rem)/2)] justify-end pr-42 xl:visible">
+      <div className="relative flex flex-col justify-end">
         <Image alt="serviceGuide" height={216} src={serviceGuide} width={360} />
         <Button
           as="a"
-          className="absolute bottom-[-10%] right-0 mr-118 w-fit rounded-30 bg-[#6D7BFF] py-16 px-32 font-pretendard text-18-bold-140 font-semibold text-white"
+          className="absolute bottom-[-13%] right-[20%] w-fit rounded-30 bg-[#6D7BFF] py-16 px-32 text-18-bold-140 font-semibold text-white"
           href={thismemeGuideUrl}
           rel="noreferrer"
           target="_blank"
@@ -21,6 +21,6 @@ export const ServiceGuide = () => {
           앱으로 사용하고 싶다면?
         </Button>
       </div>
-    </div>
+    </aside>
   );
 };

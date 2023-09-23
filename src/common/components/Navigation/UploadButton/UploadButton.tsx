151cf3f1a48b633ba79d8bfa55ede02241e1798c
elbica <sh0705k@gmail.com>
2023-09-23 22:19:44 +0900

    :sparkles: Feat: 업로드 버튼 클릭 시 페이지 이동

--- src/common/components/Navigation/UploadButton/UploadButton.tsx
+++ src/common/components/Navigation/UploadButton/UploadButton.tsx
@@ -1,6 +1,19 @@
+import { useRouter } from "next/router";
+
+import { PATH } from "@/common/utils";
+import { useAuth } from "@/features/common";
+
 export const UploadButton = () => {
+  const { validate } = useAuth();
+  const { push } = useRouter();
+  const handleClick = validate(() => {
+    push(PATH.getUploadPage());
+  });
   return (
-    <button className="rounded-20 bg-primary-700 px-16 py-8 text-14-semibold-140 text-white">
+    <button
+      className="rounded-20 bg-primary-700 px-16 py-8 text-14-semibold-140 text-white"
+      onClick={handleClick}
+    >
       업로드
     </button>
   );

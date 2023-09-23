d0c639b5ff1bac9e6d07443a841115934a250f90
elbica <sh0705k@gmail.com>
2023-09-23 22:19:44 +0900

    :sparkles: Feat: 업로드 버튼 클릭 시 페이지 이동

---
+++ src/pages/upload/index.tsx
@@ -0,0 +1,12 @@
+import { BackButtonNavigation } from "@/common/components/Navigation";
+
+const UploadPage = () => {
+  return (
+    <>
+      <BackButtonNavigation title="" />
+      upload
+    </>
+  );
+};
+
+export default UploadPage;

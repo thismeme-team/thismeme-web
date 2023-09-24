8ed8551bd220952f2932eaa411f83763d9fc57fa
elbica <sh0705k@gmail.com>
2023-09-25 01:02:27 +0900

    :recycle: Refactor: UploadMeme 마크업 구조 변경

--- src/features/upload/components/UploadMeme.tsx
+++ src/features/upload/components/UploadMeme.tsx
@@ -1,78 +1,33 @@
-import { Content, Header, Item, Root, Trigger } from "@radix-ui/react-accordion";
-
 import { Icon } from "@/common/components/Icon";
 import { Photo } from "@/common/components/Photo";
+import { UploadImage } from "@/features/upload/components/UploadImage";
+import { UploadMemeData } from "@/features/upload/components/UploadMemeData";
 
 interface Props {
   src?: string;
+  isFocus?: boolean;
 }
-export const UploadMeme = ({ src }: Props) => {
-  return (
-    <div
-      className="group w-full rounded-24 border bg-white pt-16 pb-24 focus-within:border-primary-500"
-      // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
-      tabIndex={0}
-    >
-      <div className="flex flex-col gap-24 px-16">
-        <button className="w-24">
-          <Icon color="gray-600" name="meatball" />
-        </button>
-        <Photo className="w-full rounded-16" />
 
-        <div className="flex items-center gap-8 py-8 group-[:not(:focus-within)]:hidden">
-          <Photo className="h-24 w-24 rounded-12 bg-gray-300" src={src} />
-          <span className="text-14-semibold-140 text-gray-900">분노하는 ISTJ</span>
-        </div>
-        <div className="relative w-full text-18-semibold-140 group-[:not(:focus-within)]:hidden">
-          <input
-            className="peer w-full border-b border-gray-200 px-4 pb-4 placeholder:text-gray-500 focus:outline-none"
-            placeholder="제목 작성"
-            type="text"
-          />
-          <span className="absolute inset-y-0 left-[7.5rem] text-secondary-700 peer-[:not(:placeholder-shown)]:opacity-0">
-            *
-          </span>
-        </div>
-      </div>
-      <Root
-        className="w-full border-t border-gray-100 group-[:not(:focus-within)]:hidden"
-        type="multiple"
+export const UploadMeme = ({ src, isFocus }: Props) => {
+  const canHidden = !!src;
+  return (
+    <section className="relative">
+      <button className="absolute top-16 left-16">
+        <Icon color="gray-600" name="meatball" />
+      </button>
+      <div
+        // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
+        tabIndex={0}
+        className={`group w-full rounded-24 border bg-white pt-64 pb-16 ${
+          canHidden && "focus-within:border-primary-500 focus:outline-none"
+        }`}
+        ref={(e) => {
+          if (isFocus) e?.focus();
+        }}
       >
-        <Item value="밈 출처">
-          <Header className="border-b border-gray-100">
-            <Trigger className="flex w-full items-center justify-between gap-8 rounded-full px-24 py-16 text-16-semibold-140 [&>span>#chevronDown]:data-[state=open]:rotate-180">
-              <Photo className="h-24 w-24 rounded-12" />
-              <span className="flex-grow text-left text-16-semibold-140">밈 출처</span>
-              <span className="flex h-24 w-24 items-center justify-center rounded-full hover:bg-gray-100">
-                <Icon
-                  aria-hidden
-                  className="transition-transform duration-300 ease-[cubic-bezier(0.87,0,0.13,1)]"
-                  id="chevronDown"
-                  name="chevronDown"
-                />
-              </span>
-            </Trigger>
-          </Header>
-          <Content>hello</Content>
-        </Item>
-        <Item value="밈 사용상황">
-          <Header className="border-b border-gray-100">
-            <Trigger className="flex w-full items-center justify-between gap-8 rounded-full px-24 py-16 text-16-semibold-140 [&>span>#chevronDown]:data-[state=open]:rotate-180">
-              <Photo className="h-24 w-24 rounded-12" />
-              <span className="flex-grow text-left text-16-semibold-140">밈 사용상황</span>
-              <span className="flex h-24 w-24 items-center justify-center rounded-full hover:bg-gray-100">
-                <Icon
-                  aria-hidden
-                  className="transition-transform duration-300 ease-[cubic-bezier(0.87,0,0.13,1)]"
-                  id="chevronDown"
-                  name="chevronDown"
-                />
-              </span>
-            </Trigger>
-          </Header>
-          <Content>hello</Content>
-        </Item>
-      </Root>
-    </div>
+        {src ? <Photo className="mx-16 rounded-16" src={src} /> : <UploadImage />}
+        <UploadMemeData className={canHidden ? "group-[:not(:focus-within)]:hidden" : ""} />
+      </div>
+    </section>
   );
 };
--- src/features/upload/components/UploadMemeData.tsx
+++ src/features/upload/components/UploadMemeData.tsx
@@ -1,37 +1,31 @@
 import { Content, Header, Item, Root, Trigger } from "@radix-ui/react-accordion";
-import type { ChangeEventHandler } from "react";
 
 import { Icon } from "@/common/components/Icon";
 import { Photo } from "@/common/components/Photo";
-import { UploadImage } from "@/features/upload/components/UploadImage";
 
 interface Props {
-  handleUploadImage?: ChangeEventHandler<HTMLInputElement>;
+  src?: string;
+  className?: string;
 }
-export const UploadInitialForm = ({ handleUploadImage }: Props) => {
+
+export const UploadMemeData = ({ src, className }: Props) => {
   return (
-    <div className="w-full rounded-24 border bg-white pt-16 pb-24 ">
-      <div className="flex flex-col gap-24 px-16 pb-24">
-        <button className="w-24">
-          <Icon color="gray-600" name="meatball" />
-        </button>
-        <UploadImage />
-        <div className="flex items-center gap-8 py-8">
-          <Photo className="h-24 w-24 rounded-12 bg-gray-300" />
-          <span className="text-14-semibold-140 text-gray-900">분노하는 ISTJ</span>
-        </div>
-        <div className="relative w-full text-18-semibold-140">
-          <input
-            className="peer w-full border-b border-gray-200 px-4 pb-4 placeholder:text-gray-500 focus:outline-none"
-            placeholder="제목 작성"
-            type="text"
-          />
-          <span className="absolute inset-y-0 left-[7.5rem] text-secondary-700 peer-[:not(:placeholder-shown)]:opacity-0">
-            *
-          </span>
-        </div>
+    <div className={`flex flex-col gap-24 pt-24 pb-6 ${className}`}>
+      <div className="flex items-center gap-8 py-8 px-16">
+        <Photo className="h-24 w-24 rounded-12 bg-gray-300" src={src} />
+        <span className="text-14-semibold-140 text-gray-900">분노하는 ISTJ</span>
       </div>
-      <Root className="w-full border-t border-gray-100" type="multiple">
+      <div className="relative w-full px-16 text-18-semibold-140 leading-[160%]">
+        <input
+          className="peer w-full border-b border-gray-200 px-4 pb-4 placeholder:text-gray-500 focus:outline-none"
+          placeholder=" "
+          type="text"
+        />
+        <span className="pointer-events-none absolute inset-y-0 left-20 text-gray-500 peer-[:not(:placeholder-shown)]:opacity-0">
+          제목 작성 <span className="text-secondary-700">*</span>
+        </span>
+      </div>
+      <Root className="w-full border-t border-gray-100 " type="multiple">
         <Item value="밈 출처">
           <Header className="border-b border-gray-100">
             <Trigger className="flex w-full items-center justify-between gap-8 rounded-full px-24 py-16 text-16-semibold-140 [&>span>#chevronDown]:data-[state=open]:rotate-180">

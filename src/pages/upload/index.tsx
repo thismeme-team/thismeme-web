25dea62929d31e9fe82cd76ae08214913c51a830
elbica <sh0705k@gmail.com>
2023-09-25 01:42:51 +0900

    :lipstick: Style: 밈 추가 업로드 버튼 제작

--- src/pages/upload/index.tsx
+++ src/pages/upload/index.tsx
@@ -1,17 +1,19 @@
 import { UploadNavigation } from "@/common/components/Navigation";
-import { UploadMeme } from "@/features/upload/components";
+import { AdditionalUploadBtn, UploadMeme } from "@/features/upload/components";
 
 const UploadPage = () => {
   /**
    * @todo 밈 입력폼 상태관리
    */
+
   return (
     <>
       <UploadNavigation />
       <div className="flex flex-col gap-16 pt-16 pb-60">
         <UploadMeme src="" />
         <UploadMeme isFocus src="https://picsum.photos/200/100" />
-        <UploadMeme src="https://picsum.photos/200/150" />
+        {/*<UploadMeme src="https://picsum.photos/200/150" />*/}
+        <AdditionalUploadBtn className="mx-auto" />
       </div>
     </>
   );

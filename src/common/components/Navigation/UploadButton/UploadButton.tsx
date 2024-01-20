c62c51db6a7b8f97e2c9189b4506f7844b972130
Jeongjin Oh <rojay.developer@gmail.com>
2024-01-21 00:49:07 +0900

    fix: 업로드 버튼 클릭 시 모달 나오도록 수정

--- src/common/components/Navigation/UploadButton/UploadButton.tsx
+++ src/common/components/Navigation/UploadButton/UploadButton.tsx
@@ -1,14 +1,18 @@
-import { useRouter } from "next/router";
+import { useOverlay } from "@/common/hooks";
 
-import { PATH } from "@/common/utils";
-import { useAuth } from "@/features/common";
+import { UploadModal } from "./UploadModal";
 
 export const UploadButton = () => {
-  const { validate } = useAuth();
-  const { push } = useRouter();
-  const handleClick = validate(() => {
-    push(PATH.getUploadPage());
-  });
+  const overlay = useOverlay();
+  const handleClick = () => {
+    /**
+     * TODO
+     * 업로드 기능 완료 후,
+     * 1. 업로드 페이지로 이동하는 버튼으로 변경
+     * 2. UploadModal 삭제
+     */
+    overlay.open(({ isOpen, close }) => <UploadModal open={isOpen} onClose={close} />);
+  };
   return (
     <button
       className="rounded-20 bg-primary-700 px-16 py-8 text-14-semibold-140 text-white"
